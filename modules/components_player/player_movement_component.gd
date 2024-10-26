e09c4146e24cc7644be83b2d179065181457011d
AlpAnka <alperen.alsn@gmail.com>
2024-10-26 16:57:33 +0300

    Bounce player back into screen
    Adjustments on player movement.
    Make player bounce back to screen if attempt to leave the screen.

--- modules/components_player/player_movement_component.gd
+++ modules/components_player/player_movement_component.gd
@@ -1,14 +1,20 @@
 class_name PlayerMovementController
 extends Node
 
+
+var actor: Player
+
 var speed_init: float = 400.0
 var speed_max: float = 800.0
 var speed_current: float = speed_init
 var acceleration: float = 20.0
 
-var direction: Vector2
+var limit_x_right: int
+var limit_x_left: int
+var in_screen: bool # BUG Not sure if we need this
+var can_move: bool
 
-var actor: Player
+var direction: Vector2
 
 
 func _ready() -> void:
@@ -16,7 +22,7 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	_player_movment()
+	_player_movement()
 
 
 func _initialize_controller() -> void:
@@ -25,16 +31,57 @@ func _initialize_controller() -> void:
 	actor = get_owner()
 	
 	if actor == null: push_error("Unable to get player node!")
+	
+	# Get the size of active viewport.
+	# As aspect is "keep", this means that it'll be a fixed width.
+	limit_x_right = int(get_viewport().get_visible_rect().size.x)
+	limit_x_left = 10
+	
+	in_screen = true
+	can_move = true
 
 
-func _player_movment() -> void:
-	actor.velocity = speed_current * direction
-	actor.move_and_slide()
+func _player_movement() -> void:
+	if can_move:
+		actor.velocity = speed_current * direction
+		actor.move_and_slide()
+	if _check_player_pos_limit() != Vector2.ZERO:
+		can_move = false
+		in_screen = false
+		_bounce_back_to_limit()
 
 
+## Check if player is trying to leave the screen
+func _check_player_pos_limit() -> Vector2:
+	## Player pushed through left
+	if actor.global_position.x <= limit_x_left:
+		return Vector2.RIGHT
+	## Player pushed through right
+	if actor.global_position.x >= limit_x_right:
+		return Vector2.LEFT
+	else:
+		return Vector2.ZERO
 
 
-
+## Push player ship back to mid-screen.
+func _bounce_back_to_limit():
+	if can_move and in_screen:
+		return
+	
+	var target_dir: Vector2 = _check_player_pos_limit()
+	var bounce_distance: float = 50 * target_dir.x
+	var current_pos: Vector2 = actor.global_position
+	var target_pos: Vector2 = current_pos + Vector2(bounce_distance, 0)
+	
+	var tween: Tween = create_tween()
+	tween.tween_property(actor, "global_position", target_pos, 0.2).\
+	set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+	await tween.finished
+	
+	in_screen = true
+	await get_tree().create_timer(0.5).timeout
+	can_move = true
+	# TODO Add a shake anim until you can move?
 
 
 ## Smooth out movement and include acceleration
